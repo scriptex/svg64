5ed20c65f08755e28a154209abddf996d615a77a
Atanas Atanasov <scriptex.bg@gmail.com>
2018-07-02 09:29:31 +0300

    Update source

--- src/svg64.js
+++ src/svg64.js
@@ -1,6 +1,20 @@
+/**
+ * External dependencies
+ */
 const btoa = require('btoa');
+
+/**
+ * base64 prefix
+ */
 const PREFIX = 'data:image/svg+xml;base64,';
 
+/**
+ * Detect the type of the provided SVG
+ *
+ * @param  {String|SVGElement} input
+ *
+ * @return {String}
+ */
 const detectInputType = input => {
 	if (typeof input === 'string') {
 		return 'string';
@@ -9,12 +23,15 @@ const detectInputType = input => {
 	if (typeof SVGElement !== 'undefined' && input instanceof SVGElement) {
 		return 'element';
 	}
-
-	if (typeof input.name === 'string') {
-		return 'file';
-	}
 };
 
+/**
+ * Convert SVGElement to base64
+ *
+ * @param  {SVGElement} element
+ *
+ * @return {String}
+ */
 const convertElement = element => {
 	const XMLS = new XMLSerializer();
 	const svg = XMLS.serializeToString(element);
@@ -22,8 +39,22 @@ const convertElement = element => {
 	return getBase64(svg);
 };
 
+/**
+ * Get the base64 representation of an SVG string or element
+ *
+ * @param  {Strong} svg - Serialized SVG element or SVG string
+ *
+ * @return {String}
+ */
 const getBase64 = svg => `${PREFIX}${btoa(svg)}`;
 
+/**
+ * Actually convert the provided SVG
+ *
+ * @param  {String|SVGElement} svg
+ *
+ * @return {String}
+ */
 const SVG64 = svg => {
 	const type = detectInputType(svg);
 
